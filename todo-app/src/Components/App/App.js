47bd8dea18489724f2f288d2e06f99e23e23f024
Cdrew92 <39171327+Cdrew92@users.noreply.github.com>
2021-01-23 16:04:21 -0600

    added functionality to add todo items to list

--- todo-app/src/Components/App/App.js
+++ todo-app/src/Components/App/App.js
@@ -1,25 +1,45 @@
 import logo from '../../logo.svg';
 import './App.css';
 import React from 'react';
+import AddTodo from '../AddTodo/AddTodo';
+import TodoList from '../TodoList/TodoList'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ListOfTodos: ['TEST1', 'TEST2'],
+      TodoInput: '',
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+    this.AddTodo = this.AddTodo.bind(this);
+  }
+
+  handleInput(input) {
+    this.setState({
+      TodoInput: input,
+    });
+  }
+
+  AddTodo() {
+    let TodoInput = this.state.TodoInput;
+    let Todos = this.state.ListOfTodos;
+    Todos.push(TodoInput);
+    this.setState({ListOfTodos: Todos})
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1>ToDo List</h1>
+        <div className="list-container">
+          <AddTodo AddTodo={this.AddTodo} handleInput={this.handleInput}/>
+          <TodoList List={this.state.ListOfTodos}/>
+        </div>
         </header>
       </div>
     );
