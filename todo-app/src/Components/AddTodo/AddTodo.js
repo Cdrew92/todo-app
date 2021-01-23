19cf24fc0988f6bf70902959adfa57756fa391b8
Cdrew92 <39171327+Cdrew92@users.noreply.github.com>
2021-01-23 16:02:42 -0600

    Handles adding todo items to list

---
+++ todo-app/src/Components/AddTodo/AddTodo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './AddTodo.css';
+
+
+class AddTodo extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(e) {
+        this.props.handleInput(e.target.value)
+    }
+
+  render() {
+    return (
+      <div className="Add-todo">
+          <input onChange={this.handleInput} className="Add-todo-input"></input>
+          <div className="Button-container">
+          <button onClick={this.props.AddTodo} type="submit" className="Add-todo-button">Add To List</button>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default AddTodo;
