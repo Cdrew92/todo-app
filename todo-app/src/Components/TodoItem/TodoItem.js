3c796be53b7d8fd0b941cce4e2dc079901bf980e
Cdrew92 <39171327+Cdrew92@users.noreply.github.com>
2021-01-23 21:55:32 -0600

    output date todoItem was added to list

--- todo-app/src/Components/TodoItem/TodoItem.js
+++ todo-app/src/Components/TodoItem/TodoItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from '../TodoList/TodoList';
 import './TodoItem.css';
 class TodoItem extends React.Component {
     constructor(props) {
@@ -13,13 +14,16 @@ class TodoItem extends React.Component {
 
   render() {
     return (
-    <div className="Todo-item">
-        <input disabled value={this.props.Item} className="Todo-item-input"></input>
-        <div className="Remove-button-container">
-          <button onClick={this.RemoveTodo} type="submit" className="Remove-todo-button">X</button>
+        <div className="Todo-item">
+            <input disabled value={this.props.Item.input} className="Todo-item-input"></input>
+            <div className="Date-Added">
+                <p>Date Added: {this.props.Item.DateAdded}</p>
+            </div>
+            <div className="Remove-button-container">
+            <button onClick={this.RemoveTodo} type="submit" className="Remove-todo-button">X</button>
+            </div>
         </div>
-    </div>
-    );
+    )
   }
 }
 
